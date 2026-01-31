ac28b6bc1f688b7ca638ac463f313271a87d173c
Bongo <bongopoyo@proton.me>
2026-01-31 17:24:46 +0500

    Cli.sh

--- cli.sh
+++ cli.sh
@@ -4,27 +4,34 @@
 echo -e "\033[38;2;245;194;231m"
 
 lines=(
-"██████   ██████  ███    ██  ██████   ██████"
-"██   ██ ██    ██ ████   ██ ██    ██ ██    ██"
-"██████  ██    ██ ██ ██  ██ ██       ██    ██"
-"██   ██ ██    ██ ██  ██ ██ ██ █████ ██    ██"
-"██████   ██████  ██   ████  ███████  ██████"
-
-
+"██████   ██████  ███    ██  ██████   ██████ \n"
+"██   ██ ██    ██ ████   ██ ██    ██ ██    ██ \n"
+"██████  ██    ██ ██ ██  ██ ██       ██    ██ \n"
+"██   ██ ██    ██ ██  ██ ██ ██ █████ ██    ██ \n"
+"██████   ██████  ██   ████  ███████  ██████ \n"
 )
-cols=$(tput cols)
 
 for line in "${lines[@]}"; do
-    line_length=$(echo -n "$line" | wc -m)
-    padding=$(( (cols - line_length) / 2 ))
-    printf "%*s%s\n" $padding "" "$line"
+    printf "$line"
 done
 
 echo -e "\033[0m"
+echo -e "\e[38;2;166;227;161m"
+echo "┌[About]──────────────────────────────┐ "
+echo "│                                     │ "
+echo "│ Hello, my name is Hashir (Bongo).   │ "
+echo "│ I am a rust enthusiast, artist      │ "
+echo "│ and game dev. I am very pleased to  │ "
+echo "│ meet you                            │ "
+echo "│                                     │ "
+echo "└─────────────────────────────────────┘ "
+echo -e "\e[38;2;243;139;168m"
+echo "┌[Socials]────┬───────────────────────────────────┐ "
+echo "│ itch        │https://bongopoyo.itch.io/         │ "
+echo "│ youtube     │https://www.youtube.com/@bongopoyo │ "
+echo "│ email       │bongopoyo@proton.me                │ "
+echo "│ matrix      │@bongopoyo:matrix.org              │ "
+echo "└─────────────┴───────────────────────────────────┘ "
 
-echo "Hello, My name is Hashir (Bongo)."
-echo "Visit my sites:"
-echo "> https://bongopoyo.itch.io/"
-
-
+echo -e "\033[0m"
 
